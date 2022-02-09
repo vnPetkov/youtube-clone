60086a94d71414a4cdc4987c4785691064e5ca1b
kik094 <kik094@abv.bg>
2022-02-09 12:18:40 +0200

    stylizating header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styles from "./Header.module.css";
 import header_logo from "../../images/header_logo.svg";
-import { FaMicrophone, FaSearch } from "react-icons/fa";
-import { GiHamburgerMenu } from "react-icons/gi";
+import header_delete from "../../images/header_delete.png"
+import { FaMicrophone, FaRegUserCircle } from "react-icons/fa";
+import { BsSearch, BsThreeDotsVertical } from 'react-icons/bs';
+import MenuIcon from '@mui/icons-material/Menu';
+import AppsIcon from '@mui/icons-material/Apps';
 
 
 
@@ -13,22 +16,33 @@ export default function Header() {
         <div className={styles.header}>
 
             <div className={styles.header_burger_logo}>
-                <span><GiHamburgerMenu /></span>
+                <span><MenuIcon /></span>
                 <div>
-                    <img src={header_logo}></img>
+                    <img src={header_logo} alt="logo"></img>
                 </div>
             </div>
 
             <div className={styles.header_search}>
-                <input placeholder='Search'></input>
-                <a><FaSearch /></a>
-                <span><FaMicrophone /></span>
+                <div className={styles.header_search_input}>
+                    <input placeholder='Search'></input>
+                    {/* This will be hidden when input is empty */}
+                    <span className={styles.header_search_del}>
+                        <img src={header_delete} alt="delete" />
+                    </span>
+
+                </div>
+                <span className={styles.header_searach_icon}><BsSearch /></span>
+                <span className={styles.header_searach_microphone}><FaMicrophone /></span>
 
             </div>
 
             <div className={styles.header_signin}>
-
-                <a>Sign in</a>
+                <span className={styles.header_signin_apps} fon><AppsIcon/></span>
+                <span className={styles.header_sigin_dots}><BsThreeDotsVertical /></span>
+                <div className={styles.header_singin_user}>
+                    <span className={styles.header_sigin_user_logo}><FaRegUserCircle /></span>
+                    <span className={styles.header_sigin_user_text}>SIGN IN</span>
+                </div>
             </div>
 
 
